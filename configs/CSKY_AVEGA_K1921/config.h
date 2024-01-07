bf04a01bec9b007d8d1079b258380f20a6a211b8
AlexandrShipovsky <shipovsky@csky.space>
2024-01-07 20:43:49 +0300

    init K1921

---
+++ configs/CSKY_AVEGA_K1921/config.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define FC_TARGET_MCU     STM32F427
+
+#define BOARD_NAME        AVEGA_K1921
+#define MANUFACTURER_ID   CSKY
+
+#define PINIO1_PIN           PA9
+#define PINIO1_CONFIG PINIO_CONFIG_OUT_INVERTED
+
+#define SYSTEM_HSE_MHZ 8
+
